37a27d9e24a6be2ab60c378c8c548ac5377e4f72
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-11-08 15:22:37 -0500

    things

---
+++ rust/guessing_game/src/main.rs
@@ -0,0 +1,43 @@
+use std::io;
+use std::io::Write;
+use std::process;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Guess the number");
+
+    loop {
+        print!("\nPlease input your guess: ");
+        io::stdout().flush().expect("Failed to flush stdout buffer");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    if guess.trim().eq("exit") {
+                        process::exit(0x0100);
+                    }
+                    println!("Please input a number");
+                    continue;
+                }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You guessed it");
+                break;
+            }
+        }
+    }
+
+}
