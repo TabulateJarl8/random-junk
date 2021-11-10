518d2aaf75012724a85335e42de78fd4df1b48f7
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-11-09 20:18:46 -0500

    things

--- rust/structs_test/src/main.rs
+++ rust/structs_test/src/main.rs
@@ -7,11 +7,19 @@ struct Circle {
 
 impl Circle {
     fn circumference(&self) -> f32 {
-        return 2.0 * self.radius * PI;
+        2.0 * self.radius * PI
     }
 
     fn area(&self) -> f32 {
-        return PI * (f32::powf(self.radius, 2.0));
+        PI * (f32::powf(self.radius, 2.0))
+    }
+
+    fn can_fit_in(&self, circ: &Circle) -> bool {
+        circ.radius > self.radius
+    }
+
+    fn can_contain(&self, circ: &Circle) -> bool {
+        circ.radius < self.radius
     }
 }
 
@@ -19,6 +27,15 @@ fn main() {
     let circ = Circle {
         radius: 5.0,
     };
-    println!("Circumference: {}", circ.circumference());
-    println!("Area: {}", circ.area());
+    let circ2 = Circle {
+        radius: 6.0,
+    };
+    println!("Circumference small: {}", circ.circumference());
+    println!("Area small: {}\n", circ.area());
+    println!("Circumference large: {}", circ2.circumference());
+    println!("Area large: {}\n", circ2.area());
+    println!("Small fits in large? {}", circ.can_fit_in(&circ2));
+    println!("Large fits in small? {}", circ2.can_fit_in(&circ));
+    println!("Can contain small? {}", circ2.can_contain(&circ));
+    println!("Can contain large? {}", circ.can_contain(&circ2));
 }
