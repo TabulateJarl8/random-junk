e558e2c46970c9078e3a8785747b4653e79a9e22
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-11-11 12:57:38 -0500

    windows support probably

--- rust/hangman/src/main.rs
+++ rust/hangman/src/main.rs
@@ -7,6 +7,13 @@ use std::io::Write;
 mod hangman_generator;
 
 fn main() {
+    #[cfg(windows)]
+    {
+        let enable = ansi_term::enable_ansi_support();
+        drop(enable);
+    }
+
+
     println!("{}\n", Style::new().underline().paint("-- RUST HANGMAN --"));
     let words_string = include_str!("words.txt");
     let words: Vec<&str> = words_string.split("\n").collect();
