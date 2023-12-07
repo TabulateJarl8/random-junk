9760e6b36d50786948ddb58b57e93e33adfd4f28
TabulateJarl8 <tabulatejarl8@gmail.com>
2023-12-07 17:19:47 -0500

    finish cch 7

--- rust/shuttle-cch23/src/day7.rs
+++ rust/shuttle-cch23/src/day7.rs
@@ -1,18 +1,22 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use base64::{engine::general_purpose, Engine as _};
 use rocket::{
     get,
     http::CookieJar,
     routes,
-    serde::{self, json::{Value, json}},
+    serde::{
+        self,
+        json::{json, Value},
+    },
 };
 
 fn base64_to_string(encoded: &str) -> Result<Value, anyhow::Error> {
-    let bytes = general_purpose::STANDARD
-        .decode(encoded)?;
+    let bytes = general_purpose::STANDARD.decode(encoded)?;
 
-    Ok(serde::json::from_str(&std::str::from_utf8(&bytes)?.to_string())?)
+    Ok(serde::json::from_str(
+        &std::str::from_utf8(&bytes)?.to_string(),
+    )?)
 }
 
 #[get("/7/decode")]
@@ -21,30 +25,66 @@ fn decode_cookie(cookies: &CookieJar<'_>) -> Value {
     base64_to_string(cookie_encoded.value()).unwrap()
 }
 
-
 #[get("/7/bake")]
 fn bake_cookies(cookies: &CookieJar<'_>) -> Value {
     let cookie_stats = base64_to_string(cookies.get("recipe").unwrap().value()).unwrap();
-    println!("{:?}", cookie_stats);
     let recipe = &cookie_stats["recipe"];
     let pantry = &cookie_stats["pantry"];
 
-    // TODO: fix bonus #3
-    let ingredients: Vec<&String> = recipe.as_object().unwrap().keys().collect();
+    // create sets of recipe and pantry ingredients
+    let recipe_ingredients: HashSet<&str> = recipe
+        .as_object()
+        .unwrap()
+        .keys()
+        .map(|s| s.as_str())
+        .collect();
+    let pantry_ingredients: HashSet<&str> = pantry
+        .as_object()
+        .unwrap()
+        .keys()
+        .map(|s| s.as_str())
+        .collect();
 
-    let num_cookies = ingredients.iter().map(|&item| pantry[item].as_u64().unwrap() / recipe[item].as_u64().unwrap()).min().unwrap();
-    
-    let mut new_pantry: HashMap<&str, u64> = HashMap::new();
+    // create intersection of the two to find the ingredients we need to iterate over
+    let ingredients_intersection: HashSet<&str> = recipe_ingredients
+        .intersection(&pantry_ingredients)
+        .into_iter()
+        .map(|s| *s)
+        .collect();
 
-    for item in ingredients {
-        new_pantry.insert(item, pantry[item].as_u64().unwrap() - recipe[item].as_u64().unwrap() * num_cookies);
+    // check that the intersection matches the entire recipe, if not, return nothing changed
+    if !ingredients_intersection.is_superset(&recipe_ingredients) {
+        return json!({"cookies": 0, "pantry": pantry});
+    }
+
+    let num_cookies = ingredients_intersection
+        .iter()
+        .map(|&item| {
+            pantry.get(item).unwrap_or(&json!(-1)).as_i64().unwrap()
+                / recipe.get(item).unwrap_or(&json!(-1)).as_i64().unwrap()
+        })
+        .min()
+        .unwrap();
+
+    let mut new_pantry: HashMap<&str, i64> = HashMap::new();
+
+    // check that all ingredients were able to be fullfilled
+    if num_cookies <= 0 {
+        return json!({"cookies": 0, "pantry": pantry});
+    }
+
+    // everything is valid, calculate what is lost from the pantry
+    for item in ingredients_intersection {
+        new_pantry.insert(
+            item,
+            pantry[item].as_i64().unwrap() - recipe[item].as_i64().unwrap() * num_cookies,
+        );
     }
 
     json!({
         "cookies": num_cookies,
         "pantry": new_pantry,
     })
-
 }
 
 pub fn stage() -> rocket::fairing::AdHoc {
